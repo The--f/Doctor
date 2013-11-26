26e9ecac96f2cf93a5a980f94ff8e74aeb5c396f
The--f <feker.feker@gmail.com>
2013-11-26 01:52:01 +0100

    Fix Htaccess to ommit the needed (index.php) in url

--- application/controllers/TestList.php
+++ application/controllers/TestList.php
@@ -22,15 +22,14 @@ class TestList extends CI_Controller {
         $this->load->library('grocery_CRUD');
     }
 
-    function index() {
-         $this->grocery_crud->set_table('patients');
+    function patients() {
+        $this->grocery_crud->set_table('patients');
         $output = $this->grocery_crud->render();
-         echo "<pre>";
-        print_r($output);
-        echo "</pre>";
-        die();
+        $this->_example_output($output);
+    }
 
-        //$this->_example_output($output);
+    function _example_output($output = null) {
+        $this->load->view('grocery_view', $output);
     }
 
 }
