076ee5509f45f710bbc3fcf08e589c28ea4e8e74
The--f <feker.feker@gmail.com>
2013-12-13 12:31:37 +0100

    Gestion de reservations : work in progress 30%

--- application/config/config.php
+++ application/config/config.php
@@ -223,8 +223,9 @@ $config['cache_path'] = '';
 | If you use the Encryption class or the Session class you
 | MUST set an encryption key.  See the user guide for info.
 |
-*/
-$config['encryption_key'] = '';
+ */
+// key = md5("doctor") not to be changed
+$config['encryption_key'] = 'f9f16d97c90d8c6f2cab37bb6d1f1992';
 
 /*
 |--------------------------------------------------------------------------
@@ -252,7 +253,7 @@ $config['sess_use_database']	= FALSE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
-$config['sess_time_to_update']	= 300;
+$config['sess_time_to_update'] = 100;
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/test_control.php
+++ application/controllers/test_control.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of test_control
  * un simple controlleur pour test
@@ -17,11 +11,18 @@ class test_control extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->library('session');
+        $this->load->database();
+        $this->load->model('Patient');
     }
 
     function index() {
-       echo "index_test";
 
+
+       $this->session->set_userdata( array (
+        'user_id' => 7,
+        'user_name' => 'feker',
+        'user_mail' => ''
+        ));
         echo date("Y");
         echo date("m");
         echo date("d");
